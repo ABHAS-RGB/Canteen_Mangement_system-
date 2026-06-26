67329a533d2e8c90f2ad9dffe1bcc06b1e9c4d16
ABHAS-RGB <abhas4361@gmail.com>
2026-06-26 15:37:54 +0530

    Fix app.js routes and server.js structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-const adminRoutes = require('./routes/admin');
-app.use('/api/admin', adminRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-const staffRoutes = require('./routes/staff');
-app.use('/api/staff', staffRoutes);
-const walletRoutes = require("./routes/walletRoutes");
-app.use("/api/wallet", walletRoutes);
